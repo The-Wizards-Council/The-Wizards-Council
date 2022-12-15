2c8a97040b1d67deec20dc52ed502b0fc03cd4d2
Azeb Haile <azebhaile90@gmail.com>
2022-12-15 14:30:11 -0500

    added some changes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { seed } = require("./data/index");
 
-const {Spell} = require("./models/Spell");
-const {Wizard} = require("./models/Wizard");
-const bcrypt = require('bcrypt');
-
+const { Spell } = require("./models/Spell");
+const { Wizard } = require("./models/Wizard");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3011;
@@ -12,23 +11,28 @@ const port = 3011;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-  // POST /register
+// POST /register
 // TODO - takes req.body of { student_name, isStudent, hogwartsHouse,password} and creates a new user with the hashed password
 app.post("/register", async (req, res, next) => {
   try {
-    let { student_name, isStudent, hogwartsHouse,password } = req.body;
+    let { student_name, isStudent, hogwartsHouse, password } = req.body;
     //create the salt
     let salt = await bcrypt.genSalt(5);
-   
+
     //use bcrypt to hash the password
     const hashedPw = await bcrypt.hash(password, salt);
 
     //add user to db
-   let createdUser= await Wizard.create( { student_name, isStudent, hogwartsHouse,password:hashedPw });
-   console.log(createdUser);
-   if(createdUser){
-    res.send(`successfully created new wizard ${student_name}`)
-   }
+    let createdUser = await Wizard.create({
+      student_name,
+      isStudent,
+      hogwartsHouse,
+      password: hashedPw,
+    });
+    console.log(createdUser);
+    if (createdUser) {
+      res.send(`successfully created new wizard ${student_name}`);
+    }
   } catch (error) {
     console.error(error);
     next(error);
@@ -36,73 +40,69 @@ app.post("/register", async (req, res, next) => {
 });
 
 app.post("/login", async (req, res, next) => {
-    try{
-      let { student_name, password } = req.body;
-      //search db and find where username matches what is passed in
-      let loginUser= await Wizard.findOne({
-         where: { student_name
-        } 
-      });
-      if(loginUser){
-        let isMatching = await bcrypt.compare(password, loginUser.password);
-        if(isMatching){
-          res.send(`successfully logged in user ${student_name}`);
-        }else{
-          res.send(401, 'incorrect username or password');
-        }
-        }
-      }catch(error){
-        console.error(error);
-        next(error);
-      }
+  try {
+    let { student_name, password } = req.body;
+    //search db and find where username matches what is passed in
+    let loginUser = await Wizard.findOne({
+      where: { student_name },
     });
-
-
- //Yohonna: get all wizards
- app.get("/wizards", async(req, res) => {
-    let wizard = await Wizard.findAll(req.params.id);
-    res.send(wizard)
+    if (loginUser) {
+      let isMatching = await bcrypt.compare(password, loginUser.password);
+      if (isMatching) {
+        res.send("loginsuccessful");
+      } else {
+        res.send(401, "incorrect username or password");
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
-//Yohonna:get wizards by id 
-   app.get("/wizards/:id", async (req, res) => {
-    console.log(req.params.id)
-    let wizard = await Wizard.findByPk(req.params.id);
-    res.send(wizard)
+//Yohonna: get all wizards
+app.get("/wizards", async (req, res) => {
+  let wizard = await Wizard.findAll(req.params.id);
+  res.send(wizard);
 });
 
+//Yohonna:get wizards by id
+app.get("/wizards/:id", async (req, res) => {
+  console.log(req.params.id);
+  let wizard = await Wizard.findByPk(req.params.id);
+  res.send(wizard);
+});
 
-//Yohonna:get spells by id 
+//Yohonna:get spells by id
 app.get("/spells/:id", async (req, res) => {
-    console.log(req.params.id)
-    let spell = await Spell.findByPk(req.params.id);
-    res.send(spell)
+  console.log(req.params.id);
+  let spell = await Spell.findByPk(req.params.id);
+  res.send(spell);
 });
 
-//Yohonna:get all spells 
-app.get("/spells", async(req, res) => {
-    let spell = await Spell.findAll(req.params.id);
-    res.send(spell)
+//Yohonna:get all spells
+app.get("/spells", async (req, res) => {
+  let spell = await Spell.findAll(req.params.id);
+  res.send(spell);
 });
 
 //Kris POST routes
-  //  Kris: POST new wizards
- app.post("/wizards", async(req, res, next) => {
-    
+//  Kris: POST new wizards
+app.post("/wizards", async (req, res, next) => {
   try {
-  const wizard = await Wizard.create(req.body);
-  res.send(wizard);
-   } catch (error) {
-  next(error);
+    const wizard = await Wizard.create(req.body);
+    res.send(wizard);
+  } catch (error) {
+    next(error);
   }
 });
-//Kris: POST new spells 
-app.post("/spells", async(req, res, next) => {
+//Kris: POST new spells
+app.post("/spells", async (req, res, next) => {
   try {
-  const spell = await Spell.create(req.body);
-  res.send(spell);
+    const spell = await Spell.create(req.body);
+    res.send(spell);
   } catch (error) {
-  next(error);
+    next(error);
   }
 });
 
@@ -131,7 +131,6 @@ app.put("/spells/:id", async (req, res, next) => {
     next(error);
   }
 });
-
 
 //delete route done by Ahn 12/2/2022
 app.delete("/wizards/:id", async (req, res) => {
