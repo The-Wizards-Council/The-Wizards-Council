e1324a31b31c1b2aa44bb92ff5495a18936a8112
Ahn Shields <ahnshields@gmail.com>
2022-12-16 18:39:34 -0800

    Add authorization to the delete route

--- server.js
+++ server.js
@@ -1,108 +1,159 @@
 const express = require("express");
-const { seed } = require("./data/index");
-
-const {Spell} = require("./models/Spell");
-const {Wizard} = require("./models/Wizard");
-const bcrypt = require('bcrypt');
-
-
 const app = express();
-const port = 3011;
 
+// This is for the database
+const { seed } = require("./data/index");
+const { Spell } = require("./models/Spell");
+const { Wizard } = require("./models/Wizard");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { PORT, ACCESS_TOKEN_SECRET } = process.env;
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-  // POST /register
-// TODO - takes req.body of { student_name, isStudent, hogwartsHouse,password} and creates a new user with the hashed password
-app.post("/register", async (req, res, next) => {
+// Middleware is just functions that run between your request and response.
+// Middleware needs to know if it's finished, "next" is the way to let the middleware know it's finished
+
+// Ahn: Create custom middleware authUser
+let authUser = async (req, res, next) => {
+  // Authorization comes from the header of the request object, particularly in the "Authorization" part.
+  const auth = req.header("Authorization");
+  console.log("Auth: ", auth);
+
+  if (!auth) {
+    console.log("The wizard isn't authorized...");
+    next(); // move on to the next function
+  } else {
+    console.log("The wizard is Authorized");
+
+    // Array Deconstruction, we don't need the Bearer part, only need token
+    const [, token] = auth.split(" "); // spliting the authentication string by space
+
+    // Check the validity of the token
+    const wizard = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+    req.wizard = wizard;
+    next();
+  }
+};
+
+// Yohonna: POST /register
+// TODO - takes req.body of { student_name, isStudent, isSuperUser, hogwartsHouse,password} and creates a new user with the hashed password
+app.post("/wizards/register", async (req, res, next) => {
   try {
-    let { student_name, isStudent, isSuperUser, hogwartsHouse,password } = req.body;
+    let { student_name, isStudent, isSuperUser, hogwartsHouse, password } =
+      req.body;
     //create the salt
     let salt = await bcrypt.genSalt(5);
-   
+
     //use bcrypt to hash the password
     const hashedPw = await bcrypt.hash(password, salt);
 
     //add user to db
-   let createdUser= await Wizard.create( { student_name, isStudent, isSuperUser, hogwartsHouse,password:hashedPw });
-   console.log(createdUser);
-   if(createdUser){
-    res.send(`successfully created new wizard ${student_name}`)
-   }
+    let createdUser = await Wizard.create({
+      student_name,
+      isStudent,
+      isSuperUser,
+      hogwartsHouse,
+      password: hashedPw,
+    });
+    console.log(createdUser);
+
+    // Ahn: Create a token, To sign we must include: 1. The object  2. The secret
+    const token = jwt.sign(
+      { id: createdUser.id, student_name: createdUser.student_name },
+      ACCESS_TOKEN_SECRET
+    );
+
+    res.send({
+      messge: `New wizard ${student_name} Successfully Registered`,
+      token,
+    });
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
 
-app.post("/login", async (req, res, next) => {
-    try{
-      let { student_name, password } = req.body;
-      //search db and find where username matches what is passed in
-      let loginUser= await Wizard.findOne({
-         where: { student_name
-        } 
-      });
-      if(loginUser){
-        let isMatching = await bcrypt.compare(password, loginUser.password);
-        if(isMatching){
-          res.send(`successfully logged in user ${student_name}`);
-        }else{
-          res.send(401, 'incorrect username or password');
-        }
-        }
-      }catch(error){
-        console.error(error);
-        next(error);
-      }
+// Yohonna: Login
+// Ahn: Add middleware as the second argument, authorization
+app.post("/wizards/login", authUser, async (req, res, next) => {
+  try {
+    let { student_name, password } = req.body;
+    //search db and find where username matches what is passed in
+    let loginUser = await Wizard.findOne({
+      where: { student_name },
     });
 
+    // Authenticate the loginUser
+    let isMatching = await bcrypt.compare(password, loginUser.password);
+    if (isMatching) {
+      // If True, the loginUser successfully logged in.
 
- //Yohonna: get all wizards
- app.get("/wizards", async(req, res) => {
-    let wizard = await Wizard.findAll(req.params.id);
-    res.send(wizard)
+      // Time to authorize your permissions now
+      const { id, student_name } = loginUser;
+      let payload = { id, student_name };
+
+      // Generate a token with payload and a secret
+      const token = jwt.sign(payload, ACCESS_TOKEN_SECRET);
+
+      res.send({ message: "Successful Login", token });
+    } else {
+      res.send("Please enter the correct password and try again.");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
-//Yohonna:get wizards by id 
-   app.get("/wizards/:id", async (req, res) => {
-    console.log(req.params.id)
-    let wizard = await Wizard.findByPk(req.params.id);
-    res.send(wizard)
+//Yohonna: get all wizards
+app.get("/wizards", async (req, res) => {
+  let wizard = await Wizard.findAll(req.params.id);
+  res.send(wizard);
 });
 
+//Yohonna:get wizards by id
+app.get("/wizards/:id", async (req, res) => {
+  console.log(req.params.id);
+  let wizard = await Wizard.findByPk(req.params.id);
+  res.send(wizard);
+});
 
-//Yohonna:get spells by id 
+//Yohonna:get spells by id
 app.get("/spells/:id", async (req, res) => {
-    console.log(req.params.id)
-    let spell = await Spell.findByPk(req.params.id);
-    res.send(spell)
+  console.log(req.params.id);
+  let spell = await Spell.findByPk(req.params.id);
+  res.send(spell);
 });
 
-//Yohonna:get all spells 
-app.get("/spells", async(req, res) => {
-    let spell = await Spell.findAll(req.params.id);
-    res.send(spell)
+//Yohonna:get all spells
+app.get("/spells", async (req, res) => {
+  let spell = await Spell.findAll(req.params.id);
+  res.send(spell);
 });
 
 //Kris POST routes
-  //  Kris: POST new wizards
- app.post("/wizards", async(req, res, next) => {
-    
+//  Kris: POST new wizards
+app.post("/wizards", async (req, res, next) => {
   try {
-  const wizard = await Wizard.create(req.body);
-  res.send(wizard);
-   } catch (error) {
-  next(error);
+    const wizard = await Wizard.create(req.body);
+    res.send(wizard);
+  } catch (error) {
+    next(error);
   }
 });
-//Kris: POST new spells 
-app.post("/spells", async(req, res, next) => {
+//Kris: POST new spells
+app.post("/spells", async (req, res, next) => {
   try {
-  const spell = await Spell.create(req.body);
-  res.send(spell);
+    const spell = await Spell.create(req.body);
+    res.send(spell);
   } catch (error) {
-  next(error);
+    next(error);
   }
 });
 
@@ -132,12 +183,25 @@ app.put("/spells/:id", async (req, res, next) => {
   }
 });
 
+// Ahn: Delete route 12/2/2022
+// Ahn: Add authorization 12/15
+app.delete("/wizards/:id", authUser, async (req, res) => {
+  const wizard = req.wizard;
+  const { isStudent } = req.body;
+  console.log("wizard: ", wizard);
 
-//delete route done by Ahn 12/2/2022
-app.delete("/wizards/:id", async (req, res) => {
-  const wizard = await Wizard.findByPk(req.params.id);
-  const deletedWizard = await wizard.destroy();
-  res.send(deletedWizard);
+  try {
+    const userToDelete = await Wizard.findByPk(req.params.id);
+    // Only authorized and isStudent user can delete
+    if (wizard.id == req.params.id && isStudent) {
+      const deletedUser = await userToDelete.destroy();
+      res.send(deletedUser);
+    } else {
+      res.status(401).send("Not Authorized!");
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.delete("/spells/:id", async (req, res) => {
@@ -145,7 +209,7 @@ app.delete("/spells/:id", async (req, res) => {
   const deletedSpell = await spell.destroy();
   res.send(deletedSpell);
 });
-app.listen(port, () => {
+app.listen(PORT, () => {
   seed();
-  console.log(`App listening on http://localhost:${port}`);
+  console.log(`App listening on http://localhost:${PORT}`);
 });
